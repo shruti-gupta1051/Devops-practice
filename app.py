f7981786feb90a8b731535985c5243366d086a51
shruti-gupta1051 <shruti.gupta1051@gmail.com>
2024-09-30 17:07:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    a()
     @app.route('/')
     def home():
         return 'Hi Devops Geeks Welcome to the class123 New Data 123456 New PR'
 
-
+    return app
 
     
 if __name__ == '__main__':
